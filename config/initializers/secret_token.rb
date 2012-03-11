651b66617513cfc986ddae6e131c676d71791eaf
frodriguez33 <frodriguez33@hotmail.com>
2012-03-11 12:19:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nagster::Application.config.secret_token = 'aaa46fcfb165b3cc480a31d225c06e9d6c0e8f2657263061396dbb02cfc7098a228a4939c7fcb72ee67f3054159b009e2133d965a3a5d90fbfd741fb31a36cfb'
