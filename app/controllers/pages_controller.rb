2f0b726ecd19e61b21522d38f6fe7265db3c6b69
frodriguez33 <frodriguez33@hotmail.com>
2012-03-11 15:30:15 -0700

    Set up for dynamic pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,42 @@
 class PagesController < ApplicationController
   def Home
+    @title = "Home"
   end
 
   def Contact
+    @title = "Contact"
   end
 
   def About
+	@title = "About"
+
   end
 
   def Register
+    @title = "Register"
   end
 
   def LogOn
+    @title = "LogOn"
   end
 
   def Main
+    @title = "Main"
   end
 
   def NagFriends
+    @title = "NagFriends"
   end
 
   def AddSearchFriends
+    @title = "AddSearchFriends"
   end
 
   def ViewOtherPersons
+    @title = "ViewOtherPersons"
   end
 
   def NagPopUp
+    @title = "NagPopUp"
   end
 end
