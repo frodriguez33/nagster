c1aed9ff367d76e2a396ee7c9bcab9d6edfa6b6b
frodriguez33 <frodriguez33@hotmail.com>
2012-03-12 11:52:17 -0700

    Done with the basic layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,21 +22,26 @@ class PagesController < ApplicationController
 
   def Main
     @title = "Main"
+	render :layout => 'application2'
   end
 
   def NagFriends
     @title = "NagFriends"
+	render :layout => 'application2'
   end
 
   def AddSearchFriends
     @title = "AddSearchFriends"
+	render :layout => 'application2'
   end
 
   def ViewOtherPersons
     @title = "ViewOtherPersons"
+	render :layout => 'application2'
   end
 
   def NagPopUp
     @title = "NagPopUp"
+	render :layout => 'application2'
   end
 end
